7cbaf2f0e505a4acccecf9080b60df3d04a40254
dhanalkshmi <vihaanbalaji24@gmail.com>
2025-10-07 01:27:13 +0530

    Create Sample.java
    new java file

---
+++ Sample.java
@@ -0,0 +1,20 @@
+
+public class Sample {
+	String brand="BMW";
+	String model="Sedans";
+
+	public void start() {
+		System.out.println("Car started");
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c = new Car();
+		c.start();
+		System.out.println(c.brand);
+		System.out.println(c.model);
+	}
+
+}
